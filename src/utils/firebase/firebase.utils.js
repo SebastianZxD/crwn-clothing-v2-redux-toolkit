0c6f1cdc8677c5047b40a9cdc8deb72747e5e3af
Sebas Z <juanzambrano97@hotmail.com>
2023-05-29 08:57:21 -0500

    Added redux toolkit app reducers

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyCxGXvZUNGbcJoqLxI9aYSVPucadKW10ws",
+  authDomain: "liryqal.firebaseapp.com",
+  projectId: "liryqal",
+  storageBucket: "liryqal.appspot.com",
+  messagingSenderId: "141986049795",
+  appId: "1:141986049795:web:97f876f2d8e1bdcb244336"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
